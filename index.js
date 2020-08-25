f5b262899e7072c7bf65590388a9b26a4d1c0819
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-08-25 23:21:12 +0300

    after add js

---
+++ index.js
@@ -0,0 +1,6 @@
+var faveFlowe=prompt('what is your Favorite flower ?');
+var favColor=prompt('what is your Favorite color ?');
+var name=prompt('what is your name ?');
+alert('Hello  '+name+ " we are glad about you and we hope to help you to find your Favorite flower as you loved" +' that your Favorite color is '+favColor+ 'your Favorite falower is '+faveFlowe + "And we'll save you your wish soon.");
+
+prompt
