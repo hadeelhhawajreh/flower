f341489d197b16c8e3049d37105fec6aee0225a2
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-08-27 02:00:48 +0300

    after adding if statment and dynamic js and function

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ var favColor=prompt('what is your Favorite color ?');
 var name=prompt('what is your name ?');
 alert('Hello  '+name+ " we are glad about you and we hope to help you to find your Favorite flower as you loved" +' that your Favorite color is '+favColor+ 'your Favorite falower is '+faveFlowe + "And we'll save you your wish soon.");
 
-prompt
+
+
+
+function any(folwer) {
+   
+
+   
+    if(folwer=="lilac"){
+    document.write("Your Favorite flower is here !!!");}
+    else if(folwer=="orchid"){
+        document.write("Your Favorite flower is here !!!");
+    }
+    else{
+       document.getElementById('ppp').innerHTML='Hello  '+name+ " we are glad about you and we hope to help you to find your Favorite flower as you loved" +' that your Favorite color is '+favColor+ 'your Favorite falower is '+faveFlowe + "And we'll save you your wish soon.";
+
+
+
+    }
+}
+any(faveFlowe);
